1146bd8aad8248091b92a4dc3dddfa8651b1a220
ayatsai <ctsai1011@hotmail.com>
2013-11-13 10:09:27 -0800

    update nios2 network manager after testing

--- android/Battleship/src/com/group10/battleship/MainActivity.java
+++ android/Battleship/src/com/group10/battleship/MainActivity.java
@@ -6,6 +6,7 @@ import java.net.UnknownHostException;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.group10.battleship.network.NIOS2NetworkManager;
 import com.group10.battleship.network.NetworkManager;
 import com.group10.battleship.network.NetworkManager.OnGameFoundListener;
 import com.group10.battleship.network.NetworkManager.OnIPFoundListener;
@@ -20,68 +21,69 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends SherlockActivity implements OnClickListener, OnIPFoundListener, OnGameFoundListener, OnNetworkErrorListener {
+public class MainActivity extends SherlockActivity implements OnClickListener,
+		OnIPFoundListener, OnGameFoundListener, OnNetworkErrorListener {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
-	
+
 	private Button mStartGameBtn;
 	private Button mFindGameBtn;
-	
+
 	private EditText mHostIpEt;
 	private EditText mHostPortEt;
-	
-	private TextView mHostIpTv;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mStartGameBtn = (Button)findViewById(R.id.btn_start_game);
-        mStartGameBtn.setOnClickListener(this);
-        
-        mFindGameBtn = (Button)findViewById(R.id.btn_find_game);
-        mFindGameBtn.setOnClickListener(this);
-        
-        mHostIpEt = (EditText)findViewById(R.id.et_host_ip);
-        mHostPortEt = (EditText)findViewById(R.id.et_host_port);
-        
-        mHostIpTv = (TextView)findViewById(R.id.tv_host_ip);
-    }
-    
-    @Override
-    public void onResume() {
-    	super.onResume();
-    	// Setting visibility of components. Prefs could have changed
-    	PrefsManager pm = PrefsManager.getInstance();
-    	if ( pm.getBoolean(PrefsManager.PREF_KEY_LOCAL_DEBUG, false) ||
-    			!pm.getBoolean(PrefsManager.PREF_KEY_USE_NIOS, true)) {
-    		// This should be the current ip
-    		mHostIpTv.setText("current ip here");
-        	mStartGameBtn.setVisibility(View.VISIBLE);
-        } else {
-    		// This should be the current ip
-    		mHostIpTv.setText(R.string.main_menu_nios_ip);
-        	mStartGameBtn.setVisibility(View.GONE);
-        }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getSupportMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-    	if (item.getItemId() == R.id.app_settings) {
-    		startActivity(new Intent(this, PreferenceActivity.class));
-    		return true;
-    	}
-    	return super.onOptionsItemSelected(item);
-    }
+	private TextView mHostIpTv;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mStartGameBtn = (Button) findViewById(R.id.btn_start_game);
+		mStartGameBtn.setOnClickListener(this);
+
+		mFindGameBtn = (Button) findViewById(R.id.btn_find_game);
+		mFindGameBtn.setOnClickListener(this);
+
+		mHostIpEt = (EditText) findViewById(R.id.et_host_ip);
+		mHostPortEt = (EditText) findViewById(R.id.et_host_port);
+
+		mHostIpTv = (TextView) findViewById(R.id.tv_host_ip);
+
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// Setting visibility of components. Prefs could have changed
+		PrefsManager pm = PrefsManager.getInstance();
+		if (pm.getBoolean(PrefsManager.PREF_KEY_LOCAL_DEBUG, false)
+				|| !pm.getBoolean(PrefsManager.PREF_KEY_USE_NIOS, true)) {
+			// This should be the current ip
+			mHostIpTv.setText("current ip here");
+			mStartGameBtn.setVisibility(View.VISIBLE);
+		} else {
+			// This should be the current ip
+			mHostIpTv.setText(R.string.main_menu_nios_ip);
+			mStartGameBtn.setVisibility(View.GONE);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getSupportMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.app_settings) {
+			startActivity(new Intent(this, PreferenceActivity.class));
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	public void onClick(View view) {
@@ -89,38 +91,44 @@ public class MainActivity extends SherlockActivity implements OnClickListener, O
 		if (view == mStartGameBtn) {
 			if (pm.getBoolean(PrefsManager.PREF_KEY_LOCAL_DEBUG, false)) {
 				startActivity(new Intent(this, GameActivity.class));
-			} else if (pm.getBoolean(PrefsManager.PREF_KEY_USE_NIOS, true)){
-				Toast.makeText(this, "Enter a host ip, or turn on local debugging in settings.",
+			} else if (pm.getBoolean(PrefsManager.PREF_KEY_USE_NIOS, true)) {
+				Toast.makeText(
+						this,
+						"Enter a host ip, or turn on local debugging in settings.",
 						Toast.LENGTH_LONG).show();
 				// TODO check for game connection, etc.
 			} else {
 				// Not using nios
 				try {
-					//setup host
-					NetworkManager.getInstance().setupAndroidSocket(null, 0, true);
+					// setup host
+					NetworkManager.getInstance().setupAndroidSocket(null, 0,
+							true);
 					NetworkManager.getInstance().setOnIPFoundListener(this);
 					NetworkManager.getInstance().setOnGameFoundListener(this);
-					NetworkManager.getInstance().setOnNetworkErrorListener(this);
+					NetworkManager.getInstance()
+							.setOnNetworkErrorListener(this);
 				} catch (Exception e) {
-					Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+					Toast.makeText(this, "Error: " + e.getMessage(),
+							Toast.LENGTH_LONG).show();
 					e.printStackTrace();
-				} 
+				}
 			}
 		} else if (view == mFindGameBtn) {
-			Toast.makeText(this, "Finding game...",
-					Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "Finding game...", Toast.LENGTH_SHORT).show();
 			try {
-				NetworkManager.getInstance().setupAndroidSocket(mHostIpEt.getText().toString(), 
-					Integer.parseInt(mHostPortEt.getText().toString()), false); //setup client
-			}
-			catch(NumberFormatException e)
-			{
-				Toast.makeText(this, "No Port specified", Toast.LENGTH_LONG).show();
+				NetworkManager.getInstance().setupAndroidSocket(
+						mHostIpEt.getText().toString(),
+						Integer.parseInt(mHostPortEt.getText().toString()),
+						false); // setup client
+			} catch (NumberFormatException e) {
+				Toast.makeText(this, "No Port specified", Toast.LENGTH_LONG)
+						.show();
 			} catch (Exception e) {
-				Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+				Toast.makeText(this, "Error: " + e.getMessage(),
+						Toast.LENGTH_LONG).show();
 				e.printStackTrace();
 			}
-			
+
 			// TODO
 		}
 	}
@@ -128,9 +136,10 @@ public class MainActivity extends SherlockActivity implements OnClickListener, O
 	@Override
 	public void onIPFound(String IP, int port) {
 		// TODO Auto-generated method stub
-		mHostIpTv.setText("IP: " + NetworkManager.getInstance().getAndroidHostIP() + ":" 
-				+ NetworkManager.getInstance().getAndroidHostPort() );
-		
+		mHostIpTv.setText("IP: "
+				+ NetworkManager.getInstance().getAndroidHostIP() + ":"
+				+ NetworkManager.getInstance().getAndroidHostPort());
+
 	}
 
 	@Override
@@ -142,9 +151,9 @@ public class MainActivity extends SherlockActivity implements OnClickListener, O
 
 	@Override
 	public void onClientSocketError() {
-		Toast.makeText(this, "Error: Could not find game", Toast.LENGTH_LONG).show();
-		
+		Toast.makeText(this, "Error: Could not find game", Toast.LENGTH_LONG)
+				.show();
+
 	}
 
-    
 }
