8376f3bf29e9b2a6074fbdf5e5bede7265c3bb4e
Wesley Tsai <tsai.wesley@gmail.com>
2013-11-13 17:28:13 -0800

    changed boards so they're side by side

--- module2nios/software/BattleshipNios/src/game/GameBoard.c
+++ module2nios/software/BattleshipNios/src/game/GameBoard.c
@@ -38,34 +38,29 @@ void GameBoard_drawGrid()
 	int yPosToDraw = GAMEBOARD_TOP_PADDING;
 
 	//Draw Vertical Lines
-	for( i = 0; i <= GAMEBOARD_LENGTH; i++)
+	for( i = 0; i <= GAMEBOARD_LENGTH*2+1; i++)
 	{
-		//Red on top
-		drawLineForeground(xPosToDraw, GAMEBOARD_TOP_PADDING,
-					xPosToDraw, GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*(GAMEBOARD_LENGTH)), GAMEBOARD_HOST_COLOR);
-
-		//blue on bottom
-		drawLineForeground(xPosToDraw, GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*(GAMEBOARD_LENGTH)),
-					xPosToDraw, GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*(GAMEBOARD_LENGTH*2+1)), GAMEBOARD_P2_COLOR);
-						
+		if (i <= GAMEBOARD_LENGTH) {
+		//Red to the left
+			drawLineForeground(xPosToDraw, GAMEBOARD_TOP_PADDING,
+						xPosToDraw, GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*(GAMEBOARD_LENGTH)), GAMEBOARD_HOST_COLOR);
+		} else if (i <= GAMEBOARD_LENGTH*2+1) {
+			//blue on right
+			drawLineForeground(xPosToDraw, GAMEBOARD_TOP_PADDING,
+						xPosToDraw, GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*(GAMEBOARD_LENGTH)), GAMEBOARD_P2_COLOR);
+		}
 		xPosToDraw += GAMEBOARD_COL_WIDTH;
 	}
-	
-
 
 	//Draw Horizontal Lines
-	for( i = 0; i <= GAMEBOARD_LENGTH*2; i++)
+	for( i = 0; i <= GAMEBOARD_LENGTH; i++)
 	{
-		if(i < GAMEBOARD_LENGTH)
-			drawLineForeground(GAMEBOARD_LEFT_PADDING, yPosToDraw,
-					GAMEBOARD_RIGHTMOST_COL, yPosToDraw, GAMEBOARD_HOST_COLOR); //Red
-		else if(i == GAMEBOARD_LENGTH)
-			drawLineForeground(GAMEBOARD_LEFT_PADDING, yPosToDraw,
-					GAMEBOARD_RIGHTMOST_COL, yPosToDraw, GAMEBOARD_MISS_COLOR); //Grey
-		else
-			drawLineForeground(GAMEBOARD_LEFT_PADDING, yPosToDraw,
-					GAMEBOARD_RIGHTMOST_COL, yPosToDraw, GAMEBOARD_P2_COLOR); //Blue
+		drawLineForeground(GAMEBOARD_LEFT_PADDING, yPosToDraw,
+					GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*GAMEBOARD_LENGTH), yPosToDraw, GAMEBOARD_HOST_COLOR); //Red
 						
+		drawLineForeground(GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*(GAMEBOARD_LENGTH+1)), yPosToDraw,
+							GAMEBOARD_RIGHTMOST_COL, yPosToDraw, GAMEBOARD_P2_COLOR); //Blue
+
 		yPosToDraw += GAMEBOARD_COL_HEIGHT;
 	}
 }
@@ -76,30 +71,33 @@ GameBoard_drawAlpha()
 	char number[] = "1";
 
 	int i = 0;
-	for (i = 0; i < GAMEBOARD_LENGTH*2; i++)
+	for (i = 0; i <= GAMEBOARD_LENGTH*2; i++)
 	{
 		//Print the letters on the left side
-		printString(letter, PIXEL_TO_CHAR_WIDTH*(GAMEBOARD_LEFT_PADDING-1), (PIXEL_TO_CHAR_HEIGHT*GAMEBOARD_COL_HEIGHT)+(3*i));
+		if(i < GAMEBOARD_LENGTH)
+			printString(letter, PIXEL_TO_CHAR_WIDTH*(GAMEBOARD_LEFT_PADDING)-1, (PIXEL_TO_CHAR_HEIGHT*GAMEBOARD_COL_HEIGHT)+(3*i));
 
 		//Print the numbers on the top and bottom
-		if(i < GAMEBOARD_LENGTH)
-			printString(number, (GAMEBOARD_LEFT_CHAR_PADDING+3+3*i) ,0);
-		else if(i == GAMEBOARD_LENGTH)
+		if(i == GAMEBOARD_LENGTH-1 || i == GAMEBOARD_LENGTH*2) {
 			printString("10", (GAMEBOARD_LEFT_CHAR_PADDING+3*i) ,0);
+
+		} else if(i == GAMEBOARD_LENGTH) {
+			number[0] = '0';
+		}
+		else if(i < GAMEBOARD_LENGTH*2) {
+			printString(number, (GAMEBOARD_LEFT_CHAR_PADDING+3*i) ,0);
+		}
 
 		letter[0]++;
 		number[0]++;
-
-		if(i == 9)
-			letter[0] = 'A';
 	}
 }
 
 GameBoard_drawWater()
 {
-	int xToScreen;
-	int p2YToScreen;
-	int hostYToScreen;
+	int p2XToScreen;
+	int yToScreen;
+	int hostXToScreen;
 
 
 	int x;
@@ -108,12 +106,13 @@ GameBoard_drawWater()
 	{
 		for(y = 0; y < GAMEBOARD_LENGTH; y++)
 		{
-			xToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*x);
-			p2YToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*GAMEBOARD_LENGTH)+(GAMEBOARD_COL_HEIGHT*y);
-			hostYToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*y);
+			hostXToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*x);
+			p2XToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*(GAMEBOARD_LENGTH+1))+(GAMEBOARD_COL_WIDTH*x);
+			yToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*y);
 
-			GameBoard_draw(xToScreen, p2YToScreen);
-			GameBoard_draw(xToScreen, hostYToScreen);
+
+			GameBoard_draw(hostXToScreen, yToScreen);
+			GameBoard_draw(p2XToScreen, yToScreen);
 		}
 	}
 }
@@ -143,8 +142,8 @@ void GameBoard_hostHit(GameBoard* this, int x, int y)
 void GameBoard_p2Miss(GameBoard* this, int x, int y)
 {
 	//Translate x and y boar positions to the screen
-	int xToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*x);
-	int yToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*GAMEBOARD_LENGTH)+(GAMEBOARD_COL_HEIGHT*y);
+	int xToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*GAMEBOARD_LENGTH)+(GAMEBOARD_COL_WIDTH*(x+1));
+	int yToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*y);
 
 	GameBoard_draw(xToScreen, yToScreen, MISS);
 	this->p2Board[x][y] = MISS;
@@ -153,8 +152,8 @@ void GameBoard_p2Miss(GameBoard* this, int x, int y)
 void GameBoard_p2Hit(GameBoard* this, int x, int y)
 {
 	//Translate x and y boar positions to the screen
-	int xToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*x);
-	int yToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*GAMEBOARD_LENGTH)+(GAMEBOARD_COL_HEIGHT*y);
+	int xToScreen = GAMEBOARD_LEFT_PADDING+(GAMEBOARD_COL_WIDTH*GAMEBOARD_LENGTH)+(GAMEBOARD_COL_WIDTH*(x+1));
+	int yToScreen = GAMEBOARD_TOP_PADDING+(GAMEBOARD_COL_HEIGHT*y);
 
 	GameBoard_draw(xToScreen, yToScreen, HIT);
 	this->p2Board[x][y] = HIT;
