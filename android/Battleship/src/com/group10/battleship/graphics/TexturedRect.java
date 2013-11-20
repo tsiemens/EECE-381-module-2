9baa81a77af48d0239c220c0156716911544ec09
PuiYan Denise <denisekwokk@gmail.com>
2013-11-20 12:22:29 -0800

    Reverted a var to private

--- android/Battleship/src/com/group10/battleship/graphics/TexturedRect.java
+++ android/Battleship/src/com/group10/battleship/graphics/TexturedRect.java
@@ -66,7 +66,7 @@ public class TexturedRect implements GL20Drawable{
 	private int mTextureUniformHandle;
 	private int mTextureCoordinateHandle;
 	private int mTextureDataHandle;
-	public int mTextureResId;
+	private int mTextureResId;
 	// to make sure that the texture gets changed on the GL thread
 	private int mNextTexResId;
    
